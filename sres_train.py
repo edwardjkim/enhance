24733879e0f0bb142cb97cba3010fe86d459b116
Edward J Kim <edward.junhyung.kim@gmail.com>
2017-03-10 14:07:15 -0500

    add train function

---
+++ sres_train.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def train():
+
+    raise NotImplementedError
+
+
+def main(argv=None):
+
+    train()
+
+
+if __name__ == '__main__':
+
+    tf.app.run()
